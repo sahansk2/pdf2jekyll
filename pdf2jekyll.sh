291bec51d1d0f7a1e9c439aacd5e5b1ce76df86b
Sahan Kumarasinghe <sahansk2@illinois.edu>
2020-04-12 17:08:10 -0500

    Added underscores to variables

--- pdf2jekyll.sh
+++ pdf2jekyll.sh
@@ -7,41 +7,41 @@ if [ $# -ne 1 ]; then
 	exit 1
 fi
 
-PDFPATH="$(realpath "$1")"
+PDF_PATH="$(realpath "$1")"
 # This is how Jekyll likes its filenames.
-POSTFILENAME="${DATE}-${TITLE//" "/"-"}.md"
-ASSETSFOLDERPATH="${OUTPUT}/${ASSETSFOLDER}"
-POSTFOLDERPATH="${OUTPUT}/${POSTSFOLDER}"
-POSTPATH="${OUTPUT}/${POSTSFOLDER}/${POSTFILENAME}"
+POST_FILENAME="${DATE}-${TITLE//" "/"-"}.md"
+ASSETS_FOLDER_PATH="${OUTPUT}/${ASSETS_FOLDER}"
+POSTS_FOLDER_PATH="${OUTPUT}/${POSTS_FOLDER}"
+POST_PATH="${OUTPUT}/${POSTS_FOLDER}/${POST_FILENAME}"
 
-if [ ! -d "$ASSETSFOLDERPATH" ]; then
-	mkdir -p "$ASSETSFOLDERPATH"
+if [ ! -d "$ASSETS_FOLDER_PATH" ]; then
+	mkdir -p "$ASSETS_FOLDER_PATH"
 fi
 
-if [ ! -d "$POSTFOLDERPATH" ]; then
-	mkdir -p "$POSTFOLDERPATH"
+if [ ! -d "$POSTS_FOLDER_PATH" ]; then
+	mkdir -p "$POSTS_FOLDER_PATH"
 fi
 
-echo "Creating ${POSTFILENAME}..."
-touch "$POSTPATH"
+echo "Creating ${POST_FILENAME}..."
+touch "$POST_PATH"
 
-echo "---" >> "$POSTPATH"
-echo "layout: post" >> "$POSTPATH"
-echo "title: ${TITLE}" >> "$POSTPATH"
-echo "date: ${DATE}" >> "$POSTPATH"
-echo "categories: ${CATEGORIES}" >> "$POSTPATH"
-echo "---" >> "$POSTPATH"
-echo >> "$POSTPATH"
+echo "---" >> "$POST_PATH"
+echo "layout: post" >> "$POST_PATH"
+echo "title: ${TITLE}" >> "$POST_PATH"
+echo "date: ${DATE}" >> "$POST_PATH"
+echo "categories: ${CATEGORIES}" >> "$POST_PATH"
+echo "---" >> "$POST_PATH"
+echo >> "$POST_PATH"
 
-cd "$ASSETSFOLDERPATH"
-echo "Creating PDFs into ${ASSETSFOLDERPATH} from ${PDFPATH}..."
-pdftoppm "$PDFPATH" slide -png
+cd "$ASSETS_FOLDER_PATH"
+echo "Creating PDFs into ${ASSETS_FOLDER_PATH} from ${PDF_PATH}..."
+pdftoppm "$PDF_PATH" slide -png
 cd "$OLDPWD"
 
 counter=1
-for FILE in $(cd ${ASSETSFOLDERPATH};ls -1 ${FILE});
+for FILE in $(cd ${ASSETS_FOLDER_PATH};ls -1 ${FILE});
 do
-	echo "![Page ${counter}](/${ASSETSFOLDER}/${FILE})" >> "$POSTPATH"
+	echo "![Page ${counter}](/${ASSETS_FOLDER}/${FILE})" >> "$POST_PATH"
 	((counter=counter+1))
 done
 
